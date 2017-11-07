7570a70c27f9e6def66e39c9f9d3322cbb5e7906
aforbis <aforbis83091@gmail.com>
2017-11-07 17:31:17 -0600

    Initial commit of script files for project has been worked on, but not under version control.

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+#if UNITY_EDITOR
+
+#endif
+
+namespace Assets
+{
+    public class PauseManager : MonoBehaviour {
+
+        Canvas canvas;
+
+        void Start()
+        {
+            canvas = GetComponent<Canvas>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                canvas.enabled = !canvas.enabled;
+                Pause();
+            }
+        }
+
+        public void Pause()
+        {
+            Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        }
+
+        public void Quit()
+        {
+#if UNITY_EDITOR 
+            EditorApplication.isPlaying = false;
+#else 
+		Application.Quit();
+#endif
+        }
+    }
+}
